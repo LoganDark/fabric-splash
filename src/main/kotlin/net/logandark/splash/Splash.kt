00a62a0046b5775b0ec2a112e68daac7c4e7764c
Logan Darklock <logandarklock@gmail.com>
2020-09-23 20:35:41 -0700

    only run initializer on client

--- src/main/kotlin/net/logandark/splash/Splash.kt
+++ src/main/kotlin/net/logandark/splash/Splash.kt
@@ -1,14 +1,14 @@
 package net.logandark.splash
 
-import net.fabricmc.api.ModInitializer
+import net.fabricmc.api.ClientModInitializer
 import org.apache.logging.log4j.LogManager
 import org.apache.logging.log4j.Logger
 
 @Suppress("unused")
-object Splash : ModInitializer {
+object Splash : ClientModInitializer {
 	private val LOGGER: Logger = LogManager.getLogger()
 
-	override fun onInitialize() {
+	override fun onInitializeClient() {
 		LOGGER.info("Hello Fabric world!")
 
 		SplashConfig.load()
